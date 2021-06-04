1046597776079d9e7a9935899544b5fe076e0dfb
vinassefranche <vincent.francois@inato.com>
2021-06-04 10:43:30 +0200

    Add required plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,26 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    `gatsby-plugin-mdx`,
+    {
+      resolve: `gatsby-plugin-csp`,
+      options: {
+        disableOnDev: false,
+        mergeScriptHashes: true,
+        directives: {
+          "script-src": `'self'${process.env.WITH_UNSAFE_EVAL ? " 'unsafe-eval'" : ''}`,
+          "style-src": `'self' 'unsafe-inline'`,
+          "connect-src": `'self' localhost:* ws://localhost:*`,
+        },
+      },
+    },
+  ],
 }
